27942f9d1234bd096d8ada8081907df20599cb4c
hgdevelopment <heeraerptl@gmail.com>
2017-10-30 19:01:47 +0530

    some minor modifications

--- resources/views/core.blade.php
+++ resources/views/core.blade.php
@@ -38,7 +38,7 @@
 		      <li><a href="#">About</a></li>
 		      <li><a href="#">Projects</a></li>
 		      <li><a href="#">Resume</a></li>
-		      <li><a href="#">Contacts</a></li>
+		      <li><a href="#">Contact</a></li>
 		    </ul>
 		  </div>
 		</nav>
